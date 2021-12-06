5623fa8da912e25cde253c1ddbd2ce5cbaa68375
Sagar <avhad.avhad@gmail.com>
2021-12-06 12:21:02 +0530

    variables in python

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,1 +1,9 @@
-print("Hello World")
+first_Name = "Sameer"
+last_Name = "Avhad"
+age = 26
+weight = 68.9
+is_Genius = True
+print(first_Name+" "+last_Name)
+print(age)
+print(weight)
+print(is_Genius)
