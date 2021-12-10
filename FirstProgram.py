afef9d1c4362a0f1ea5f13a16b53d83968a114aa
Sagar <avhad.avhad@gmail.com>
2021-12-10 06:10:56 +0530

    elif means agar if wali condition fail ho gayi toh koyi baat nhi ye condition check kr lo

--- FirstProgram.py
+++ FirstProgram.py
@@ -7,7 +7,6 @@ if age >= 18:
 elif age < 18 and age >3:
     print("you are in school")
 else:
-    print("you are not adult")
-    print("you can not vote")
+    print("you are a child")
 
 print("Thank you")
